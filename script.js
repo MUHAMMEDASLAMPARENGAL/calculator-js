66b995b4b6bf628d0ee82cf49c064e879c54f398
MUHAMMEDASLAMPARENGAL <58245923+MUHAMMEDASLAMPARENGAL@users.noreply.github.com>
2022-08-23 15:39:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var screen = document.getElementById("screen");
 
 function calc(value){
-    screen.innerHTML += value;
+    screen.innerText += value;
 }
 function clearscreen(){
-    screen.innerHTML ="";
+    document.getElementById("screen").innerText = screen.innerText.slice(0, - 1);
 }
 function result(){
-    var result = eval(screen.innerHTML);
-    screen.innerHTML= result;
+    var result = eval(screen.innerText);
+    screen.innerText= result;
 }
