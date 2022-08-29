ee3a7efe92c97cb8b79f1e0e6a5a3ce9e0a153b2
MUHAMMEDASLAMPARENGAL <58245923+MUHAMMEDASLAMPARENGAL@users.noreply.github.com>
2022-08-30 01:29:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,186 +1,128 @@
-const delEl = document.querySelector('#del')
-const valueEl = document.querySelector('#value');
-const additionEl = document.querySelector('.addition');
-const subtractionEl = document.querySelector('.subtraction');
-const multiplicationEl = document.querySelector('.multiplication');
-const divisionEl = document.querySelector('.division');
-const equalEl = document.querySelector('#equal');
-const operationEl = document.querySelectorAll(".operation");
-const numbersEl = document.querySelectorAll(".numbers");
+const display1El = document.querySelector(".display-1");
+        const display2El = document.querySelector("#value");
+        const tempResultEl = document.querySelector(".temp-result");
+        const numbersEl = document.querySelectorAll(".numbers");
+        const operationEl = document.querySelectorAll(".operation");
+        const equalEl = document.querySelector("#equal");
+        const clearAllEl = document.querySelector("#del");
+        const clearLastEl = document.querySelector(".last-entity-clear");
+        let dis1Num = "";
+        let dis2Num = "";
+        let result = null;
+        let lastOperation = "";
+        let haveDot = false;
 
-const decimalEl = document.querySelector('.decimal');
-const number0El = document.querySelector('.number-0');
-const number1El = document.querySelector('.number-1');
-const number2El = document.querySelector('.number-2');
-const number3El = document.querySelector('.number-3');
-const number4El = document.querySelector('.number-4');
-const number5El = document.querySelector('.number-5');
-const number6El = document.querySelector('.number-6');
-const number7El = document.querySelector('.number-7');
-const number8El = document.querySelector('.number-8');
-const number9El = document.querySelector('.number-9');
-const numberElArray = [
-    number0El, number1El, number2El, number3El, number4El,
-    number5El, number6El, number7El, number8El, number9El
-];
+        numbersEl.forEach((number) => {
+            number.addEventListener("click", (e) => {
+                if (e.target.innerText === "." && !haveDot) {
+                    haveDot = true;
+                } else if (e.target.innerText === "." && haveDot) {
+                    return;
+                }
+                dis2Num += e.target.innerText;
+                display2El.innerText = dis2Num;
+                // console.log();
+            });
+        });
 
+        operationEl.forEach((operation) => {
+            operation.addEventListener("click", (e) => {
+                if (!dis2Num) return;
+                haveDot = false;
+                const operationName = e.target.innerText;
+                if (dis1Num && dis2Num && lastOperation) {
+                    mathOperation();
+                } else {
+                    result = parseFloat(dis2Num);
+                }
+                clearVar(operationName);
+                lastOperation = operationName;
+                console.log(result);
+            });
+        });
+        function clearVar(name = "") {
+            dis1Num += dis2Num + " " + name + " ";
+            display2El.innerText = "";
+            dis2Num = "";
 
-// variables
-let valueStrInMemory = null;
-let operatorInMemory = null;
-
-
-// Functions
-const getValueAsStr = () => valueEl.textContent.split(',').join('');
-
-const getValueAsNum = () => {
-    return parseFloat(getValueAsStr());
-};
-
-const setStrAsValue = (valueStr) => {
-    if (valueStr[valueStr.length - 1] === '.') {
-        valueEl.textContent += '.';
-        return;
-    }
-
-    const [wholeNumStr, decimalStr] = valueStr.split('.');
-    if (decimalStr) {
-        valueEl.textContent =
-            parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
-    } else {
-        valueEl.textContent = parseFloat(wholeNumStr).toLocaleString();
-    }
-};
-
-const handleNumberClick = (numStr) => {
-    const currentValueStr = getValueAsStr();
-    if (currentValueStr === '0') {
-        setStrAsValue(numStr);
-    } else {
-        setStrAsValue(currentValueStr + numStr);
-    }
-};
-
-const getResultOfOperationAsStr = () => {
-    const currentValueNum = getValueAsNum();
-    const valueNumInMemory = parseFloat(valueStrInMemory);
-    let newValueNum;
-    if (operatorInMemory === 'addition') {
-        newValueNum = valueNumInMemory + currentValueNum;
-    } else if (operatorInMemory === 'subtraction') {
-        newValueNum = valueNumInMemory - currentValueNum;
-    } else if (operatorInMemory === 'multiplication') {
-        newValueNum = valueNumInMemory * currentValueNum;
-    } else if (operatorInMemory === 'division') {
-        newValueNum = valueNumInMemory / currentValueNum;
-    }
-
-    return newValueNum.toString();
-};
-
-const handleOperatorClick = (operation) => {
-    const currentValueStr = getValueAsStr();
-
-    if (!valueStrInMemory) {
-        valueStrInMemory = currentValueStr;
-        operatorInMemory = operation;
-        setStrAsValue('0');
-        return;
-    }
-    valueStrInMemory = getResultOfOperationAsStr();
-    operatorInMemory = operation;
-    setStrAsValue('0');
-};
-
-// add event listeners to operators
-additionEl.addEventListener('click', () => {
-    handleOperatorClick('addition');
-});
-subtractionEl.addEventListener('click', () => {
-    handleOperatorClick('subtraction');
-});
-multiplicationEl.addEventListener('click', () => {
-    handleOperatorClick('multiplication');
-});
-divisionEl.addEventListener('click', () => {
-    handleOperatorClick('division');
-});
-equalEl.addEventListener('click', () => {
-    if (valueStrInMemory) {
-        setStrAsValue(getResultOfOperationAsStr());
-        valueStrInMemory = null;
-        operatorInMemory = null;
-    }
-});
-
-
-// Add Event Listeners to numbers and decimal
-for (let i = 0; i < numberElArray.length; i++) {
-    const numberEl = numberElArray[i];
-    numberEl.addEventListener('click', () => {
-        handleNumberClick(i.toString());
-    });
-}
-decimalEl.addEventListener('click', () => {
-    const currentValueStr = getValueAsStr();
-    if (!currentValueStr.includes('.')) {
-        setStrAsValue(currentValueStr + '.');
-    }
-});
-
-delEl.addEventListener('click', () => {
-    valueEl.innerText = valueEl.innerText.substr(0, valueEl.innerText.length - 1);
-
-    valueStrInMemory = null;
-    operatorInMemory = null;
-});
-
-window.addEventListener("keydown", (e) => {
-    if (
-        e.key === "0" ||
-        e.key === "1" ||
-        e.key === "2" ||
-        e.key === "3" ||
-        e.key === "4" ||
-        e.key === "5" ||
-        e.key === "6" ||
-        e.key === "7" ||
-        e.key === "8" ||
-        e.key === "9" ||
-        e.key === "."
-    ) {
-        clickButtonEl(e.key);
-        // console.log(e.key)
-    } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "%") {
-        clickOperation(e.key);
-    } else if (e.key === "*") {
-        clickOperation("x");
-        // console.log(e.key)
-    } else if (e.key == "Enter" || e.key === "=") {
-        clickEqual();
-    } else if (e.key == "Backspace") {
-        valueEl.innerText = valueEl.innerText.substr(0, valueEl.innerText.length - 1);
-        valueStrInMemory = null;
-        operatorInMemory = null;
-    }
-    // console.log(e.key)
-});
-function clickButtonEl(key) {
-    numbersEl.forEach((button) => {
-        if (button.innerText === key) {
-            button.click();
         }
-    });
-}
-function clickOperation(key) {
-    operationEl.forEach((operation) => {
-        if (operation.innerText === key) {
-            operation.click();
+
+        function mathOperation() {
+            if (lastOperation === "x") {
+                result = parseFloat(result) * parseFloat(dis2Num);
+            } else if (lastOperation === "+") {
+                result = parseFloat(result) + parseFloat(dis2Num);
+            } else if (lastOperation === "-") {
+                result = parseFloat(result) - parseFloat(dis2Num);
+            } else if (lastOperation === "/") {
+                result = parseFloat(result) / parseFloat(dis2Num);
+            } else if (lastOperation === "%") {
+                result = parseFloat(result) % parseFloat(dis2Num);
+            }
         }
-    });
-}
-function clickEqual() {
-    equalEl.click();
-}
+        // operation();
+
+        equalEl.addEventListener("click", () => {
+            if (!dis2Num || !dis1Num) return;
+            haveDot = false;
+            mathOperation();
+            clearVar();
+            display2El.innerText = result;
+            dis2Num = result;
+            dis1Num = "";
+        });
+
+        clearAllEl.addEventListener("click", () => {
+            dis1Num = "";
+            dis2Num = "";
+            display2El.innerText = "0";
+            result = "";
+        });
+
+        
+
+        window.addEventListener("keydown", (e) => {
+            if (
+                e.key === "0" ||
+                e.key === "1" ||
+                e.key === "2" ||
+                e.key === "3" ||
+                e.key === "4" ||
+                e.key === "5" ||
+                e.key === "6" ||
+                e.key === "7" ||
+                e.key === "8" ||
+                e.key === "9" ||
+                e.key === "."
+            ) {
+                clickButtonEl(e.key);
+                // console.log(e.key)
+            } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "%") {
+                clickOperation(e.key);
+            } else if (e.key === "*") {
+                clickOperation("x");
+                // console.log(e.key)
+            } else if (e.key == "Enter" || e.key === "=") {
+                clickEqual();
+            }
+            // console.log(e.key)
+        });
+        function clickButtonEl(key) {
+            numbersEl.forEach((button) => {
+                if (button.innerText === key) {
+                    button.click();
+                }
+            });
+        }
+        function clickOperation(key) {
+            operationEl.forEach((operation) => {
+                if (operation.innerText === key) {
+                    operation.click();
+                }
+            });
+        }
+        function clickEqual() {
+            equalEl.click();
+        }
 
 
